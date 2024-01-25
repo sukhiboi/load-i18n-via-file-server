200f28e8acc1e47bb58436b7ac890faa61d99edb
sukhiboi <41sukhdevsingh@gmail.com>
2024-01-25 19:15:29 +0530

    Add hybrid example

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import {useTranslation} from "react-i18next";
 import {useState} from "react";
 
 const App = () => {
-    const {t, i18n} = useTranslation(["common"])
+    const {t, i18n} = useTranslation(["common", "secret"])
     const [value, setValue] = useState("");
+    const [showSecretText, setShowSecretText] = useState(false);
 
     const changeLanguage = (langCode: string) => {
         i18n.changeLanguage(langCode);
@@ -11,10 +12,17 @@ const App = () => {
     };
 
     return <div>
-        <h1>{t("awesome.text")}</h1>
+        <h1>{t("common:awesome.text")}</h1>
         <div>
             <label>Change language (lang code) </label>
-            <input name="yoooo" value={value} onChange={e => setValue(e.target.value)}/>
+            <input value={value} onChange={e => setValue(e.target.value)}/>
+
+            <div style={{marginTop: "2rem", marginBottom: "2rem"}}>
+                <h2 style={{background: showSecretText ? "white" : "grey"}}>{showSecretText ? t("secret:awesome.text") : "Very secret message"}</h2>
+                <button onClick={() => setShowSecretText(prevState => !prevState)}>
+                    Wanna see secret text!!!
+                </button>
+            </div>
 
             <div style={{marginTop: "1rem"}}>
                 <button onClick={() => changeLanguage(value)} style={{marginRight: "1rem"}}>Change</button>
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,13 +2,23 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import HttpApi from 'i18next-http-backend';
 import Language from "./Language";
+import resourcesToBackend from "i18next-resources-to-backend";
+import Backend from 'i18next-chained-backend';
+
+const inMemoryResourceBackend = resourcesToBackend(
+    (language: string, namespace: string) =>
+        import(`./locales/${language}/${namespace}.json`)
+);
 
 i18n
-    .use(HttpApi)
+    .use(Backend)
     .use(initReactI18next)
     .init({
         backend: {
-            loadPath: 'http://localhost:8000/locales/{{lng}}/{{ns}}.json',
+            backends: [inMemoryResourceBackend, HttpApi],
+            backendOptions: [{}, {
+                loadPath: 'http://localhost:8000/locales/{{lng}}/{{ns}}.json',
+            }]
         },
         ns: [],
         nsSeparator: ":",
